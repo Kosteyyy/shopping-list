4499abecda767db816e75bd53ac0d23d17a80a95
Kosteyyy <averkiev@yandex.ru>
2022-02-03 12:18:02 +0300

    fetching data to provider

---
+++ src/containers/List.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import SubHeader from '../components/Header/SubHeader';
+import ListItem from '../components/ListItem/ListItem';
+
+const ListItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 2% 5%;
+`;
+
+const Alert = styled.span`
+  width: 100%;
+  text-align: center;
+`;
+
+const List = ({ listItems, loading = false, error = false , lists, match, history }) => {
+  const items =
+    listItems && listItems.filter(item => item.listId === parseInt(match.params.id));
+    const list = lists && lists.find(list => list.id === parseInt(match.params.id));
+
+  return !loading && !error ? (
+    <>
+      {history && list && (
+        <SubHeader
+          goBack={() => history.goBack()} title={list.title}
+          openForm={() => history.push(`${match.url}/new`)}
+        />
+      )}
+      <ListItemWrapper>
+        {items && items.map(item => <ListItem key={item.id} data={item} />)}
+      </ListItemWrapper>
+    </>
+  ) : (
+    <Alert>{loading ? 'Loading...' : error}</Alert>
+  );
+};
+
+export default List;
